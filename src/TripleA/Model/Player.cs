6e0a9726fd084855f058c535be8af78713016c72
khababaa <102924962+khababaa@users.noreply.github.com>
2024-01-30 16:07:56 +0100

    stat player operationel

--- src/TripleA/Model/Player.cs
+++ src/TripleA/Model/Player.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TripleA.Model
 {
@@ -12,14 +10,45 @@ namespace TripleA.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string Pseudo { get; set; }
+        public Team Team { get; set; } 
+        public List<Statistic> Statistiques { get; set; }
 
         public Player(int id, string name, string pseudo)
         {
             Id = id;
             Name = name;
             Pseudo = pseudo;
+            Statistiques = new List<Statistic>();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        // Méthode pour obtenir les statistiques du joueur
+        public Dictionary<string, double> GetStatsPlayer()
+        {
+            int totalWins = Statistiques.Count(s => s.Winner);
+            int totalKills = Statistiques.Sum(s => s.Kills);
+            int totalDeaths = Statistiques.Sum(s => s.Death);
+            int totalAssists = Statistiques.Sum(s => s.Assists); 
+            int totalDegatInflige = Statistiques.Sum(s => s.DegatInflige);
+            int totalDegatSubit = Statistiques.Sum(s => s.DegatSubit);
+
+            double winRate = Statistiques.Count == 0 ? 0 : (double)totalWins / Statistiques.Count * 100;
+            double kda = totalDeaths == 0 ? totalKills + totalAssists : (double)(totalKills + totalAssists) / totalDeaths;
+            double dda = totalDegatSubit == 0 ? totalDegatInflige : (double)totalDegatInflige / totalDegatSubit;
+
+            return new Dictionary<string, double>
+            {
+                { "WinRate", winRate },
+                { "KDA", kda },
+                { "DDA", dda }
+            };
+        }
+
+        // Gestion des notifications de changement de propriété
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }

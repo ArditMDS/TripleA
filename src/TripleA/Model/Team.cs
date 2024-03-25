f80f07b2ca4fc6d23c0a8b4e5cc4f4cc354364ae
khababaa <102924962+khababaa@users.noreply.github.com>
2024-03-25 08:59:57 +0100

    Stats fot team up

--- src/TripleA/Model/Team.cs
+++ src/TripleA/Model/Team.cs
@@ -1,24 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TripleA.Model
 {
-    public class Team
+    public class Team : INotifyPropertyChanged
     {
-        public Guid id { get; set; }
-        public string name { get; set; }
-        public string nameTag { get; set; }
-        public List<Player> players { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string NameTag { get; set; }
+        public List<Player> Players { get; set; }
 
-        public Team(Guid anId, string aName, string aNameTag, List<Player> thePlayers)
+        public Team(Guid id, string name, string nameTag, List<Player> players)
         {
-            this.id = anId;
-            this.name = aName;
-            this.nameTag = aNameTag;
-            this.players = thePlayers;
+            Id = id;
+            Name = name;
+            NameTag = nameTag;
+            Players = players;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Méthode pour obtenir les statistiques de l'équipe
+        public Dictionary<string, double> GetStatsTeam()
+        {
+            var teamStats = Players
+                .SelectMany(player => player.Statistiques)
+                .ToList();
+
+            int totalWins = teamStats.Count(s => s.Winner);
+            int totalGames = teamStats.Count;
+            int totalKills = teamStats.Sum(s => s.Kills);
+            int totalDeaths = teamStats.Sum(s => s.Death);
+            int totalAssists = teamStats.Sum(s => s.Assists);
+            int totalDegatInflige = teamStats.Sum(s => s.DegatInflige);
+            int totalDegatSubit = teamStats.Sum(s => s.DegatSubit);
+
+            double winRate = totalGames == 0 ? 0 : (double)totalWins / totalGames * 100;
+            double kda = totalDeaths == 0 ? totalKills + totalAssists : (double)(totalKills + totalAssists) / totalDeaths;
+            double dda = totalDegatSubit == 0 ? totalDegatInflige : (double)totalDegatInflige / totalDegatSubit;
+
+            return new Dictionary<string, double>
+            {
+                { "WinRate", winRate },
+                { "KDA", kda },
+                { "DDA", dda }
+            };
+
         }
     }
 }

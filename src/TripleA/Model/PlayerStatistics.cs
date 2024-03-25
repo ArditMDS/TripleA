ef9f0528150ba1747412af5d560a71c4caf65d57
Ardit <Ardit@DESKTOP-FNR8NCQ>
2024-03-25 08:38:50 +0100

    fixed stats

--- src/TripleA/Model/PlayerStatistics.cs
+++ src/TripleA/Model/PlayerStatistics.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace TripleA.Model
 {
-    public class PlayerStatistics : Statistics
+    public class PlayerStatistics : Statistic
     {
+        public PlayerStatistics(bool winner, int kills, int death, int assists, int degatInflige, int degatSubit, int cs, TimeSpan dureePartie) : base(winner, kills, death, assists, degatInflige, degatSubit, cs, dureePartie)
+        {
+        }
     }
 }
--- src/TripleA/Model/TeamStatistics.cs
+++ src/TripleA/Model/TeamStatistics.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace TripleA.Model
 {
-    public class TeamStatistics : Statistics
+    public class TeamStatistics : Statistic
     {
+        public TeamStatistics(bool winner, int kills, int death, int assists, int degatInflige, int degatSubit, int cs, TimeSpan dureePartie) : base(winner, kills, death, assists, degatInflige, degatSubit, cs, dureePartie)
+        {
+        }
     }
 }

b60db360814d4f22c6997540644925e878229730
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-03-25 10:14:47 +0100

    add team selector on player page && manual creation of teams

--- src/TripleA/ViewModel/PlayerViewModel.cs
+++ src/TripleA/ViewModel/PlayerViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripleA.Model;
 
 namespace TripleA.ViewModel
 {
     public class PlayerViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Team> AvailableTeams { get; set; } = new ObservableCollection<Team>();
+
+        public PlayerViewModel()
+        {
+            // creation manuelle de quelques équipes
+            AvailableTeams.Add(new Team(Guid.NewGuid(), "Équipe Alpha", "ALPHA", new List<Player>()));
+            AvailableTeams.Add(new Team(Guid.NewGuid(), "Équipe Beta", "BETA", new List<Player>()));
+            AvailableTeams.Add(new Team(Guid.NewGuid(), "Équipe Gamma", "GAMMA", new List<Player>()));
+        }
+
     }
 }
--- src/TripleA/Views/PlayerPage.xaml.cs
+++ src/TripleA/Views/PlayerPage.xaml.cs
@@ -1,3 +1,6 @@
+using TripleA.Model;
+using TripleA.ViewModel;
+
 namespace TripleA.Views;
 
 public partial class PlayerPage : ContentPage
@@ -5,5 +8,12 @@ public partial class PlayerPage : ContentPage
 	public PlayerPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new PlayerViewModel();
+    }
+    private void OnTeamSelected(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var selectedTeam = (Team)picker.SelectedItem;
+    }
+
 }

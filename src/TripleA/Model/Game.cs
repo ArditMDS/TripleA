2d236c89c84270214457820808816a3ee43902bf
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-03-27 14:35:05 +0100

    modify game model

--- src/TripleA/Model/Game.cs
+++ src/TripleA/Model/Game.cs
@@ -10,14 +10,20 @@ namespace TripleA.Model
     {
         public Guid id { get; set; }
         public DateTime date { get; set; }
-        public List<Team> teams { get; set; }
+
+        // public List<Team> teams { get; set; }
+
+        public Player player1 { get; set; }
+        public Player player2 { get; set; }
         public string gameName { get; set; }
 
         public Game(Guid anId, DateTime aDate, List<Team> theTeams, string theGameName)
         {
             this.id = anId;
             this.date = aDate;
-            this.teams = theTeams;
+            // this.teams = theTeams;
+            this.player1 = player1;
+            this.player2 = player2;
             this.gameName = theGameName;
         }
     }

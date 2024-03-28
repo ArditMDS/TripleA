bee2c596b922bb9bfac05b88776dc8abfec26b7d
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-03-28 09:20:41 +0100

    disable AddPlayer btn when editing

--- src/TripleA/ViewModel/OtherViewModel.cs
+++ src/TripleA/ViewModel/OtherViewModel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripleA.Managers;
+using TripleA.Model;
 
 namespace TripleA.ViewModel
 {
-    internal class OtherViewModel
+    public class OtherViewModel
     {
+        // voici le code pour récupérer les Players stockés dans le PlayerManager
+        // dans un autre ViewModel
+
+        private readonly PlayerManager _playerManager;
+
+        public OtherViewModel(PlayerManager playerManager)
+        {
+            _playerManager = playerManager;
+        }
+
+        public ObservableCollection<Player> Players => _playerManager.Players;
     }
 }

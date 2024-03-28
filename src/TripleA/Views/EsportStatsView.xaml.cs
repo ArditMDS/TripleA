c80e10c1441c74a64a78a7eb88d4f78d2bd6600d
khababaa <102924962+khababaa@users.noreply.github.com>
2024-03-28 09:17:13 +0100

    stats with type of stats o team up to date

---
+++ src/TripleA/Views/EsportStatsView.xaml.cs
@@ -0,0 +1,16 @@
+namespace TripleA.Views;
+using TripleA.ViewModel;
+using TripleA.ViewModels;
+
+public partial class EsportStatsView : ContentPage
+{
+	public EsportStatsView()
+	{
+        InitializeComponent();
+        var viewModel = new EsportStatsViewModel();
+        this.BindingContext = viewModel;
+
+      
+       
+    }
+}

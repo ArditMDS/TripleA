e292eee2f4ec2d9dfce4bff5ddc71cf36bc9dac9
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-03-28 13:44:41 +0100

    scroll to top on click Modifier

--- src/TripleA/Views/PlayerPage.xaml.cs
+++ src/TripleA/Views/PlayerPage.xaml.cs
@@ -12,6 +12,27 @@ public partial class PlayerPage : ContentPage
 		InitializeComponent();
 
         BindingContext = App.PlayerViewModelInstance;
+
+        if (BindingContext is PlayerViewModel viewModel)
+        {
+            viewModel.ScrollToTopRequested += ViewModel_ScrollToTopRequested;
+        }
+
+    }
+
+    private async void ViewModel_ScrollToTopRequested(object sender, EventArgs e)
+    {
+        // scroll vers le top
+        await MainScrollView.ScrollToAsync(0, 0, true);
+    }
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        if (BindingContext is PlayerViewModel viewModel)
+        {
+            viewModel.ScrollToTopRequested -= ViewModel_ScrollToTopRequested;
+        }
     }
     private void OnTeamSelected(object sender, EventArgs e)
     {
